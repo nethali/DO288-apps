5fe1799e8f805fa0a0128f163268e0ed03888670
Student User <student@workstation.lab.example.com>
2024-01-04 20:47:29 -0500

    Updated php file

--- php-template/index.php
+++ php-template/index.php
@@ -1,10 +1,6 @@
 <?php
-    print "This app returns a random funny quote.\n"
-?>
-
-<?php
 $greet = getenv('HELLO_GREETING');
 $person = getenv('HELLO_MESSAGE');
 
-echo "$greet $person";
+echo "$greet $person\n";
 ?>
